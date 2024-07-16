08744a0f991554fc401c16b591dc4b7f1885d7cf
danil <danilshirokovskii@gmail.com>
2024-07-16 13:47:01 +0600

    Added AuthStore and implemented the logic of the login button

--- src/forms/AuthForm.tsx
+++ src/forms/AuthForm.tsx
@@ -1,7 +1,9 @@
 import { TextField, Button } from '@mui/material';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
-
+import { observer } from 'mobx-react-lite';
+import * as React from 'react';
+import authStore from '../stores/AuthStore';
 
 const validationSchema = yup.object({
     name: yup
@@ -12,7 +14,14 @@ const validationSchema = yup.object({
         .required('Требуется пароль'),
 });
 
-const AuthForm = () => {
+const AuthForm = observer(() => {
+    const [loginError, setLoginError] = React.useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginError('');
+        authForm.handleChange(e);
+    };
+
     const authForm = useFormik({
         initialValues: {
             name: '',
@@ -20,40 +29,48 @@ const AuthForm = () => {
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
+            authStore.login(values.name, values.password);
+            if (authStore.isAuthenticated) {
+                alert("Вы успешно вошли!")
+                setLoginError('');
+            } else {
+                setLoginError('Неправильный логин или пароль');
+            }
         },
     });
 
-    return (<form onSubmit={authForm.handleSubmit}>
-        <TextField
-            fullWidth
-            id="name"
-            name="name"
-            label="Логин"
-            variant="standard"
-            value={authForm.values.name}
-            onChange={authForm.handleChange}
-            onBlur={authForm.handleBlur}
-            error={authForm.touched.name && Boolean(authForm.errors.name)}
-            helperText={authForm.touched.name && authForm.errors.name}
-            placeholder="Введите логин"
-        />
-        <TextField
-            fullWidth
-            id="password"
-            name="password"
-            label="Пароль"
-            type="password"
-            variant="standard"
-            value={authForm.values.password}
-            onChange={authForm.handleChange}
-            onBlur={authForm.handleBlur}
-            error={authForm.touched.password && Boolean(authForm.errors.password)}
-            helperText={authForm.touched.password && authForm.errors.password}
-            placeholder="Введите пароль"
-        />
-        <Button variant="contained" type="submit" >Войти</Button>
-    </form>);
-}
+    return (
+        <form onSubmit={authForm.handleSubmit}>
+            <TextField
+                fullWidth
+                id="name"
+                name="name"
+                label="Логин"
+                variant="standard"
+                value={authForm.values.name}
+                onChange={handleChange}
+                onBlur={authForm.handleBlur}
+                error={authForm.touched.name && Boolean(authForm.errors.name) || Boolean(loginError)}
+                helperText={authForm.touched.name && authForm.errors.name}
+                placeholder="Введите логин"
+            />
+            <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Пароль"
+                type="password"
+                variant="standard"
+                value={authForm.values.password}
+                onChange={handleChange}
+                onBlur={authForm.handleBlur}
+                error={(authForm.touched.password && Boolean(authForm.errors.password)) || Boolean(loginError)}
+                helperText={(authForm.touched.password && authForm.errors.password) || loginError}
+                placeholder="Введите пароль"
+            />
+            <Button variant="contained" type="submit">Войти</Button>
+        </form>
+    );
+});
 
 export default AuthForm;
