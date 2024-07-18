5a4fc653173b32c7aa2579015ba2d955c7b1ae5b
danil <danilshirokovskii@gmail.com>
2024-07-18 11:52:13 +0600

    Update CI, Readme, add jest library for tests and format code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('admin-panel-vidial')!).render(
-  <React.StrictMode>
-  </React.StrictMode>,
-)
+  <React.StrictMode></React.StrictMode>,
+);
