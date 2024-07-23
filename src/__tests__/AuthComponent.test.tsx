b3f64ba78f6e131677dc3a940295e429fe94179f
danil <danilshirokovskii@gmail.com>
2024-07-24 01:06:12 +0600

    VIDL-266. Create WarningWindowExit and update AuthComponent.test, format code

--- src/__tests__/AuthComponent.test.tsx
+++ src/__tests__/AuthComponent.test.tsx
@@ -1,33 +1,52 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import { DefaultTheme } from 'theme/DefaultTheme';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthComponent from 'routes/AuthPage/AuthComponent';
-import { MemoryRouter } from 'react-router';
+import MainPage from 'routes/MainPage/MainPage';
+
+jest.mock('routes/AuthPage/AuthComponent', () => ({
+  ...jest.requireActual('routes/AuthPage/AuthComponent'),
+  handleLogin: jest.fn(),
+}));
 
 describe('AuthComponent', () => {
-  it('renders the logo and bro image', () => {
-    const { getByAltText } = render(
-      <MemoryRouter>
-        <ThemeProvider theme={DefaultTheme}>
-          <AuthComponent />
-        </ThemeProvider>
-      </MemoryRouter>,
+  it('renders the login form', () => {
+    render(
+      <Router>
+        <AuthComponent />
+      </Router>,
     );
 
-    expect(getByAltText('Vidial_logo')).toBeInTheDocument();
-    expect(getByAltText('Vidial_picture')).toBeInTheDocument();
+    expect(screen.getByText('Вход')).toBeInTheDocument();
   });
 
-  it('renders the login form', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <ThemeProvider theme={DefaultTheme}>
-          <AuthComponent />
-        </ThemeProvider>
-      </MemoryRouter>,
+  it('navigates to main page on successful login', async () => {
+    const { handleLogin } = require('routes/AuthPage/AuthComponent');
+
+    handleLogin.mockImplementation(() => {
+      return Promise.resolve(true);
+    });
+
+    render(
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthComponent />} />
+          <Route path="/main" element={<MainPage />} />
+        </Routes>
+      </Router>,
     );
 
-    expect(getByText('Вход')).toBeInTheDocument();
+    fireEvent.change(screen.getByPlaceholderText('Логин'), {
+      target: { value: 'user' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Пароль'), {
+      target: { value: 'password' },
+    });
+
+    fireEvent.click(screen.getByText('Войти'));
+
+    await screen.findByText('Main Page');
+
+    expect(screen.getByText('Main Page')).toBeInTheDocument();
   });
 });
--- src/layouts/SidebarComponents/SidebarMenuItems.tsx
+++ src/layouts/SidebarComponents/SidebarMenuItems.tsx
@@ -1,4 +1,4 @@
-import { Home, ContentPaste, People, ExitToApp } from '@mui/icons-material';
+import { Home, ContentPaste, People } from '@mui/icons-material';
 import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
 import MedicalServicesOutlinedIcon from '@mui/icons-material/MedicalServicesOutlined';
 import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
@@ -48,11 +48,6 @@ const menuItems: IMenuItem[] = [
     text: 'Пользователи',
     icon: <People />,
     path: '/users',
-  },
-  {
-    text: 'Выйти',
-    icon: <ExitToApp />,
-    path: '/auth',
   },
 ];
 
